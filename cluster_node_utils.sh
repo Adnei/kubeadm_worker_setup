ca9c00a0ba348fde61a320ffdb6e0ea416cc493e
Eduardo <eduardogc0303@usp.br>
2024-01-29 17:25:33 -0300

    Adding primitive wqcluster setup file

--- cluster_node_utils.sh
+++ cluster_node_utils.sh
@@ -124,81 +124,12 @@ while getopts ":hd:ai:w:c" option; do
       ;;
    c) # adds a controller and sets up a k8s based on a configuration file "conifig_init.yaml" placed in the same directory
 	   if [ -z "${dst_host}" ]; then
-        echo -e "ERROR: arg -d is required for authentication.\n"
+        echo -e "ERROR: arg -d is required for authentication.\n"echo
         echo -e "Set arg -d <user>@<host_ip> as the first argument"
         exit 1
       fi
            ## Info on what's going on (command that is being executed, etc) should be provided in some way
-		ssh ${dst_host} "
-        sudo ./node_setup.sh 'sudo kubeadm init --pod-netword-cidr=198.162.0.0/24'
-		sudo ufw disable # beware of this.
-		mkdir -p $HOME/.kube
-		sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-		sudo chown $(id -u):$(id -g) $HOME/.kube/config
-		#setting up files system for proper mounting
-		cd 
-		cd ..
-		cd ..
-		cd /var/lib/
-		sudo mkdir grafana/
-		cd
-		cd ..
-		cd ..
-		sudo mkdir prometheus/
-		#deploying Persistent Volumes
-		cd
-		cd kubeadm_worker_setup/
-		kubectl apply -f persistent-volume-instantiation-1.yaml
-		kubectl apply -f persistent-volume-instantiation-2.yaml
-		cd
-		cd kubeadm_worker_setup/
-		#setting up calico CNI
-		cd
-		cd kubeadm_worker_setup/
-		cd new_calico/
-		kubectl create -f tigera-operator.yaml
-		kubectl create -f custom-resources.yaml
-		kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-		kubectl taint nodes --all node-role.kubernetes.io/master-
-		cd
-	cd kubeadm_worker_setup/
-		#setting up prometheus
-		cd prometheus_test/
-		kubectl create namespace monitoring
-		kubectl create -f clusterRole.yaml	# it is inferred that there are the prometheus_test/ folder with the files required for a prometheus setup (for more -> https://devopscube.com/setup-prometheus-monitoring-on-kubernetes/)
-		kubectl create -f prometheus_config-map.yaml 
-		kubectl create -f prometheus-deployment-PV.yaml
-		kubectl create -f prometheus-service.yaml
-		cd
-		cd kubeadm_worker_setup/
-		# setting up grafana
-		cd grafana_tests/ # once again, it is presumed that those files will be provided in some way before running the script (for more -> https://devopscube.com/setup-grafana-kubernetes/)
-		kubectl create -f grafana-datasource-config.yaml
-		kubectl create -f grafana_dep_PERSIST.yaml
-		kubectl create -f grafana_service.yaml
-		# by default, user and password are admin/admin, the user will be prompted to change the admin password as soon as they access the grafana UI for the first time
-		cd
-		cd kubeadm_worker_setup/
-		# setting up scaphandre
-		# before setting up scaphandre, it is required to set up helm chart since this is where the scaphandre setup for k8s is located 
-		curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-		sudo apt-get install apt-transport-https --yes
-		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-		sudo apt-get update
-		sudo apt-get install helm
-		# since helm has been installed, now it is possible to set up scaphandre from its repo
-		git clone https://github.com/hubblo-org/scaphandre
-		cd scaphandre
-		git fetch
-		git switch dev
-		helm install scaphandre helm/scaphandre
-		cd
-		cd kubeadm_worker_setup/
-		# setting up the node exporter
-		cd node_exporter/
-		kubectl create -f daemonset.yaml
-		kubectl create -f node_exporter_service.yaml
-		cd"
+		ssh -t ${dst_host} "cd ~/kubeadm_worker_setup; sudo ./cluster_setup.sh" 
         ## if # (kubectl get nodes | grep ${dst_user}) | awk '{print $3}' != 'controller'
         ##  then
         ##        echo -e "Node added but role not set as controller" This would be a... interesting ... cenario, we're just making sure that everything went well here
